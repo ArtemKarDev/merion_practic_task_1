1a73ce7a6304ce31350e738b1c0e2749ce712935
ArtemKarDev <akm86@mail.ru>
2024-09-28 14:15:59 +0300

    added first test

--- src/main/java/ru.merion.aqa/lesson1/Practic1.java
+++ src/main/java/ru.merion.aqa/lesson1/Practic1.java
@@ -3,13 +3,12 @@ package ru.merion.aqa.lesson1;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import ru.merion.aqa.WebDriverFactory;
 
 import java.util.List;
 
 public class Practic1 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         WebDriver driver = WebDriverFactory.create("chrome");
 
