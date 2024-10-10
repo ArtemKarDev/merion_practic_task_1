395a16ac5fc7d20b3cd94686b28c2bc35ed7b022
ArtemKarDev <akm86@mail.ru>
2024-10-11 00:52:13 +0300

    add new lesson - ec wait

--- src/main/java/ru.merion.aqa/lesson5/CalcalatorScript.java
+++ src/main/java/ru.merion.aqa/lesson5/CalcalatorScript.java
@@ -2,7 +2,6 @@ package ru.merion.aqa.lesson5;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import ru.merion.aqa.WebDriverFactory;
@@ -17,7 +16,6 @@ public class CalcalatorScript {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         //  для явного ожидания
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-
         //  Открыть страницу
         driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html");
         //  В поле ввода по локатору #delay ввести значение 45
@@ -28,7 +26,6 @@ public class CalcalatorScript {
         driver.findElement(By.xpath("//span[text()=\"+\"]")).click();
         driver.findElement(By.xpath("//span[text()=\"8\"]")).click();
         driver.findElement(By.xpath("//span[text()=\"=\"]")).click();
-
         //  Дождаться результата.
         wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector("div.screen"),"15"));
         String solution = driver.findElement(By.cssSelector("div.screen")).getText();
--- src/main/java/ru.merion.aqa/lesson5/InternetShop.java
+++ src/main/java/ru.merion.aqa/lesson5/InternetShop.java
@@ -2,17 +2,14 @@ package ru.merion.aqa.lesson5;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import ru.merion.aqa.WebDriverFactory;
 
 import java.time.Duration;
-import java.util.List;
 
 public class InternetShop {
     public static void main(String[] args) {
-
 
         WebDriver driver = WebDriverFactory.create("chrome");
         //  неявное ожидание
