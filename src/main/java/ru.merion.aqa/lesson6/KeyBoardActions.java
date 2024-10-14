733270fdd49b7806845786a2d5f4b292c77eb083
ArtemKarDev <akm86@mail.ru>
2024-10-14 18:12:39 +0300

    add new lesson 6

---
+++ src/main/java/ru.merion.aqa/lesson6/KeyBoardActions.java
@@ -0,0 +1,45 @@
+package ru.merion.aqa.lesson6;
+
+import net.bytebuddy.implementation.bytecode.ShiftLeft;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import ru.merion.aqa.WebDriverFactory;
+
+public class KeyBoardActions {
+    public static void main (String[] args){
+
+        WebDriver driver = WebDriverFactory.create("chrome");
+        driver.get("http://uitestingplayground.com/textInput");
+
+        By locator = By.cssSelector("#newButtonName");
+
+        Keys cmdCtrl = Platform.getCurrent().is(Platform.WIN10) ? Keys.COMMAND : Keys.CONTROL;
+
+        long pause = 1L;
+        new Actions(driver)
+                .keyDown(Keys.LEFT_SHIFT)
+                .sendKeys(driver.findElement(locator), "merion")
+                .keyUp(Keys.LEFT_SHIFT)
+                .pause(pause)
+                .keyDown(Keys.LEFT_SHIFT)
+                .sendKeys(Keys.ARROW_UP)
+                .keyUp(Keys.LEFT_SHIFT)
+                .keyDown(cmdCtrl)
+                .sendKeys("c")
+                .pause(pause)
+                .sendKeys("vv")
+                .pause(pause)
+                .sendKeys("v")
+                .keyUp(cmdCtrl)
+                .pause(pause)
+                .perform();
+
+        driver.quit();
+
+    }
+
+
+}
---
+++ src/main/java/ru.merion.aqa/lesson6/MouseActions.java
@@ -0,0 +1,4 @@
+package ru.merion.aqa.lesson6;
+
+public class MouseActions {
+}
