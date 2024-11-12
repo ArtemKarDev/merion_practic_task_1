a88b90bbe84adf67f71e88e0594a9886a8d681ab
ArtemKarDev <akm86@mail.ru>
2024-11-13 00:14:43 +0300

    add spec for RestAssured

--- src/test/java/ru/merion/aqa/lesson18/contract/XClientsCompanyTest.java
+++ src/test/java/ru/merion/aqa/lesson18/contract/XClientsCompanyTest.java
@@ -1,13 +1,19 @@
 package ru.merion.aqa.lesson18.contract;
 
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import okhttp3.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import ru.merion.aqa.lesson15.model.Company;
 
 import java.io.IOException;
+import java.util.List;
 
 
 import static io.restassured.RestAssured.given;
@@ -18,24 +24,52 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class XClientsCompanyTest {
     public static final String URL = "https://x-clients-be.onrender.com";
     public static final String COMPANY_ENDPOINT = "/company";
+    public static final String DELETE_ENDPOINT = "/company/delete/{id}";
     public static final String LOGIN_ENDPOINT = "/auth/login";
-    public static final String X_CLIENT_TOKEN = "x-clients-token";
+    private static final String X_CLIENT_TOKEN = "x-clients-token";
+    private static ResponseSpecification RESPONSE_401;
+    private static RequestSpecification CREATE_REQUEST;
+    private static RequestSpecification DELETE_REQUEST;
 
 
     @BeforeAll
-    public static void detUp() {
+    public static void setUp() {
         RestAssured.baseURI = URL;
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+        RESPONSE_401 = new ResponseSpecBuilder()
+                .expectStatusCode(401)
+                .expectBody("message", equalTo("Unauthorized"))
+                .expectBody("statusCode", equalTo(401))
+                .build();
+
+        String json = """
+                {
+                  "name": "Contract Test Company",
+                  "description": "Contract Test Company"
+                }                
+                """;
+
+        CREATE_REQUEST = new RequestSpecBuilder()
+                .addHeader(X_CLIENT_TOKEN, getToken())
+                .setContentType(ContentType.JSON)
+                .setBody(json)
+                .build();
+
+        DELETE_REQUEST = new RequestSpecBuilder()
+                .addHeader(X_CLIENT_TOKEN, getToken())
+                .setBasePath(DELETE_ENDPOINT)
+                .build();
 
     }
 
     @Test
     public void shouldReturnArrayOnGetCompanyList() {
-
-        given()
+        List<Company> list = given()
                 .get(COMPANY_ENDPOINT)
                 .then()
-                .statusCode(200).extract().jsonPath().getList("", Company.class);
+                .statusCode(200)
+                .extract().jsonPath().getList("", Company.class);
 
         assertTrue(list.isEmpty());
 
@@ -43,22 +77,12 @@ public class XClientsCompanyTest {
 
     @Test
     public void shouldReturn401OWithoutToken() throws IOException{
-        String json = """
-                {
-                  "username": "leonardo",
-                  "password": "leads"
-                }                
-                """;
-        given().body(json).contentType(ContentType.JSON)
+
+        given().spec(CREATE_REQUEST)
                 .post(COMPANY_ENDPOINT)
                 .then()
-                .statusCode(401)
-                .body("statusCode", equalTo(401))
-                .body("message", equalTo("Unauthorize"));
-
+                .spec(RESPONSE_401);
     }
-
-
 
     @Test
     public void shouldReturn401WithoutValidToken() throws IOException{
@@ -69,47 +93,23 @@ public class XClientsCompanyTest {
                 }                
                 """;
 
-        given().body(json)
-                .contentType(ContentType.JSON)
+        given()
+                .spec(CREATE_REQUEST)
                 .header(X_CLIENT_TOKEN,"NON_VALID_TOKEN")
                 .post(COMPANY_ENDPOINT)
                 .then()
-                .statusCode(401)
-                .body("statusCode", equalTo(401))
-                .body("message", equalTo("Unauthorized"));
+                .spec(RESPONSE_401);
     }
 
     @Test
-    public void shouldReturn2O1OnCompanyCreated() throws IOException{
+    public void shouldReturn2O1OnCompanyCreated() {
 
-        String creds = """
-                {
-                  "name": "Contract Test Company",
-                  "description": "Contract Test Company"
-                }                
-                """;
-
-        String json = """
-                {
-                  "name": "Contract Test Company",
-                  "description": "Contract Test Company"
-                }                
-                """;
-
-        String token = given().body(creds)
-                .contentType(ContentType.JSON)
-                .post(LOGIN_ENDPOINT)
-                .then()
-                .statusCode(201)
-                .extract().path("userToken");
-
-        given().body(json)
-                .contentType(ContentType.JSON)
-                .header(X_CLIENT_TOKEN,token)
+        given()
+                .spec(CREATE_REQUEST)
                 .post(COMPANY_ENDPOINT)
                 .then()
                 .statusCode(201)
-                .body("id", greaterThanOrEqualTo(1));
+                .body("id", greaterThanOrEqualTo(0));
 
     }
 
@@ -160,21 +160,31 @@ public class XClientsCompanyTest {
 
     @Test
     @Tag("Defect")
-    public void shouldGET404OnDeleteNonExistCompany() throws IOException{
+    public void shouldGet404OnDeleteNonExistCompany() {
         int id = createDummyCompany();
 
-        given().basePath(COMPANY_ENDPOINT)
-                .contentType(ContentType.JSON)
-                .header(X_CLIENT_TOKEN,getToken())
-                .get("/delete/{id}")
+        given()
+                .spec(DELETE_REQUEST)
+                .get("",id)
                 .andReturn();
 
-        given().basePath(COMPANY_ENDPOINT)
-                .contentType(ContentType.JSON)
-                .header(X_CLIENT_TOKEN,getToken())
-                .get("/delete/{id}")
+        given()
+                .spec(DELETE_REQUEST)
+                .get("",id)
                 .then()
                 .statusCode(404);
+    }
+
+    @Test
+    public void shouldReturn401OnDeleteCompany(){
+        int id = createDummyCompany();
+
+        given()
+                .spec(DELETE_REQUEST)
+                .header(X_CLIENT_TOKEN, "")
+                .get("",id)
+                .then()
+                .spec(RESPONSE_401);
     }
 
 
